f7996a56c93fd8dd6c0e703be32afe89db4f48d6
Maximinn <138689905+Maximinn@users.noreply.github.com>
2023-11-21 10:25:38 -0500

    debug

--- frontend/src/app/container/header-container.jsx
+++ frontend/src/app/container/header-container.jsx
@@ -2,8 +2,9 @@
 import React, { Component, useState } from "react";
 import HeaderComponent from "../component/header-component";
 import LoginModal from "../component/LoginModal-component";
+import Details from "../component/detail-modal-component";
 import ListFilm from "../component/list-film-component";
-
+ 
 class HeaderContainer extends Component {
   constructor(props) {
     super(props);
@@ -11,32 +12,43 @@ class HeaderContainer extends Component {
       showLoginModal: false,
       showListFilm: false,
       isLoggedIn: false,
+      show: false,
+      movie: [],
     };
   }
-
+ 
   handleLoginClick = () => {
     this.setState({ showLoginModal: true });
   };
-
+ 
   handleCloseModal = () => {
     this.setState({ showLoginModal: false });
   };
-
+ 
   handleListFilmClick = () => {
     this.setState({ showListFilm: true });
   };
-
+ 
   handleCloseListFilm = () => {
     this.setState({ showListFilm: false });
   };
-
+ 
   handleLoginSuccess = () => {
     this.setState({ showLoginModal: false, isLoggedIn: true });
   };
-
+ 
   handleLogout = () => {
     this.setState({ isLoggedIn: false });
   };
+ 
+  showModal = (movie) => {
+    this.setState({ show: true, movie });
+  };
+ 
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+ 
   render() {
     return (
       <div>
@@ -45,13 +57,15 @@ class HeaderContainer extends Component {
           isLoggedIn={this.state.isLoggedIn}
           onLoginSuccess={this.handleLoginSuccess}
           onLogout={this.handleLogout}
+          onListFilmClick={this.handleListFilmClick}
         />
+ 
         <LoginModal
           onClose={this.handleCloseModal}
           showLoginModal={this.state.showLoginModal}
           onLoginSuccess={this.handleLoginSuccess}
         />
-
+ 
         {this.state.showListFilm ? (
           <ListFilm showDetails={this.showModal} />
         ) : null}
@@ -60,10 +74,9 @@ class HeaderContainer extends Component {
           show={this.state.show}
           movie={this.state.movie}
         />
-
       </div>
     );
   }
 }
-
+ 
 export default HeaderContainer;
